2a5d6401b1b9914f90068291aabe2359b4005492
Andrey Velichko <andrey.whiteid@gmail.com>
2022-09-10 01:50:45 +0300

    Ожидает ревью

---
+++ HomeWork_2/tasks/task1/Main.java
@@ -0,0 +1,89 @@
+package task1;
+
+/*
+    Task1
+        Убрать дубликаты, отсортировать по идентификатору, сгруппировать по имени
+        Что должно получиться
+            Key: Amelia
+            Value:4
+            Key: Emily
+            Value:1
+            Key: Harry
+            Value:3
+            Key: Jack
+            Value:1
+*/
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    static class Person {
+        final int id;
+
+        final String name;
+
+        Person(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Person person)) return false;
+            return getId() == person.getId() && getName().equals(person.getName());
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(getId(), getName());
+        }
+    }
+
+    private static Person[] RAW_DATA = new Person[]{
+            new Person(0, "Harry"),
+            new Person(0, "Harry"), // дубликат
+            new Person(1, "Harry"), // тёзка
+            new Person(2, "Harry"),
+            new Person(3, "Emily"),
+            new Person(4, "Jack"),
+            new Person(4, "Jack"),
+            new Person(5, "Amelia"),
+            new Person(5, "Amelia"),
+            new Person(6, "Amelia"),
+            new Person(7, "Amelia"),
+            new Person(8, "Amelia"),
+    };
+
+    public static void main(String[] args) {
+        System.out.println("Raw data:");
+        System.out.println();
+
+        for (Person person : RAW_DATA) {
+            System.out.println(person.id + " - " + person.name);
+        }
+
+        System.out.println();
+        System.out.println("**************************************************");
+        System.out.println();
+        System.out.println("Duplicate filtered, grouped by name, sorted by name and id:");
+        System.out.println();
+
+        Map<String, Long> personsMap = Arrays.stream(Optional.of(RAW_DATA)
+                        .orElseThrow(NullPointerException::new))
+                .distinct()
+                .sorted(Comparator.comparing(Person::getId))
+                .collect(Collectors.groupingBy(Person::getName, Collectors.counting()));
+        personsMap.forEach((key, value) ->
+                System.out.println("Key: " + key + "\nValue: " + value));
+    }
+}
